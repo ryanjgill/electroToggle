7b78a17b23833f85c83e283d2e1639ff24e07205
Ryan Gill <ryanjgill2@gmail.com>
2018-07-13 15:23:57 -0500

    adding power switch to stop/start cycle

--- electroToggle.ino
+++ electroToggle.ino
@@ -1,22 +1,30 @@
 /* Electroplating
  * 
  * Toggle relay with set on and off durations
+ * Power switch interrupt to toggle cycling of relay
  * 
  */
 
-int relay = 7;
-int led = 13;
-long onTime = 4000;
-long offTime = 4000;
+const int powerSwitch = 2;
+const int relay = 7;
+const int led = 13;
+
+long onTime = 10000;
+long offTime = 3000;
+
+volatile bool isActive = false;
 
 void turnOn() {
   digitalWrite(relay, HIGH);
-  Serial.println("Relay ON");
 }
 
 void turnOff() {
   digitalWrite(relay, LOW);
-  Serial.println("Relay OFF");
+}
+
+void toggleState() {
+  isActive = !isActive;
+  digitalWrite(led, isActive);
 }
 
 void setup()
@@ -24,19 +32,23 @@ void setup()
   // Set both relay and led pins to OUTPUT
   pinMode(relay, OUTPUT);
   pinMode(led, OUTPUT);
-
-  // Turn on power led
-  digitalWrite(led, HIGH);
+  // Set power switch pin to INPUT
+  pinMode(powerSwitch, INPUT);
+  // Default power switch to HIGH state
+  // Pressing switch brings it LOW
+  digitalWrite(powerSwitch, HIGH); 
+  // Attach an interrupt to the power switch pin
+  attachInterrupt(digitalPinToInterrupt(powerSwitch), toggleState, FALLING);
 }
 
 void loop()
 {
-  Serial.begin(9600);
-  // Turn on relay for on duration
-  turnOn();
-  delay(onTime);
-
-  // Turn off relay for off duration
-  turnOff();
-  delay(offTime);
+  while(isActive) {
+    // Turn on relay for on duration
+    turnOn();
+    delay(onTime);
+    // Turn off relay for off duration
+    turnOff();
+    delay(offTime);
+  } 
 }
